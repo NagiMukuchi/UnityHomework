ae7aac3a961329d03402d3cfaccc3894241dd6de
グエンカインアン <96474826+ChoiKanghee@users.noreply.github.com>
2025-08-27 20:03:16 +0700

    bai 2

--- Demo/Assets/BigRookGames/Scripts/Weapons/GunfireController.cs
+++ Demo/Assets/BigRookGames/Scripts/Weapons/GunfireController.cs
@@ -16,7 +16,7 @@ namespace BigRookGames.Weapons
         public GameObject muzzlePosition;
 
         // --- Config ---
-        public bool autoFire;
+        public bool autoFire;              // Bật true = bắn tự động, false = bắn khi nhấn chuột
         public float shotDelay = .5f;
         public bool rotate = true;
         public float rotationSpeed = .25f;
@@ -39,28 +39,43 @@ namespace BigRookGames.Weapons
 
         private void Start()
         {
-            if(source != null) source.clip = GunShotClip;
+            if (source != null) source.clip = GunShotClip;
             timeLastFired = 0;
             lastScopeState = scopeActive;
         }
 
         private void Update()
         {
-            // --- If rotate is set to true, rotate the weapon in scene ---
+            // --- Nếu rotate được bật thì xoay súng ---
             if (rotate)
             {
-                transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y 
-                                                                        + rotationSpeed, transform.localEulerAngles.z);
+                transform.localEulerAngles = new Vector3(
+                    transform.localEulerAngles.x,
+                    transform.localEulerAngles.y + rotationSpeed,
+                    transform.localEulerAngles.z
+                );
             }
 
-            // --- Fires the weapon if the delay time period has passed since the last shot ---
-            if (autoFire && ((timeLastFired + shotDelay) <= Time.time))
+            // --- Chế độ bắn ---
+            if (autoFire)
             {
-                FireWeapon();
+                // Auto fire (bắn liên tục theo shotDelay)
+                if ((timeLastFired + shotDelay) <= Time.time)
+                {
+                    FireWeapon();
+                }
+            }
+            else
+            {
+                // Click chuột trái để bắn
+                if (Input.GetMouseButton(0) && (timeLastFired + shotDelay <= Time.time))
+                {
+                    FireWeapon();
+                }
             }
 
-            // --- Toggle scope based on public variable value ---
-            if(scope && lastScopeState != scopeActive)
+            // --- Toggle scope ---
+            if (scope && lastScopeState != scopeActive)
             {
                 lastScopeState = scopeActive;
                 scope.SetActive(scopeActive);
@@ -96,40 +111,29 @@ namespace BigRookGames.Weapons
             // --- Handle Audio ---
             if (source != null)
             {
-                // --- Sometimes the source is not attached to the weapon for easy instantiation on quick firing weapons like machineguns, 
-                // so that each shot gets its own audio source, but sometimes it's fine to use just 1 source. We don't want to instantiate 
-                // the parent gameobject or the program will get stuck in a loop, so we check to see if the source is a child object ---
-                if(source.transform.IsChildOf(transform))
+                if (source.transform.IsChildOf(transform))
                 {
                     source.Play();
                 }
                 else
                 {
-                    // --- Instantiate prefab for audio, delete after a few seconds ---
                     AudioSource newAS = Instantiate(source);
                     if ((newAS = Instantiate(source)) != null && newAS.outputAudioMixerGroup != null && newAS.outputAudioMixerGroup.audioMixer != null)
                     {
-                        // --- Change pitch to give variation to repeated shots ---
                         newAS.outputAudioMixerGroup.audioMixer.SetFloat("Pitch", Random.Range(audioPitch.x, audioPitch.y));
                         newAS.pitch = Random.Range(audioPitch.x, audioPitch.y);
 
-                        // --- Play the gunshot sound ---
                         newAS.PlayOneShot(GunShotClip);
-
-                        // --- Remove after a few seconds. Test script only. When using in project I recommend using an object pool ---
                         Destroy(newAS.gameObject, 4);
                     }
                 }
             }
-
-            // --- Insert custom code here to shoot projectile or hitscan from weapon ---
-
         }
 
         private void ReEnableDisabledProjectile()
         {
-            reloadSource.Play();
-            projectileToDisableOnFire.SetActive(true);
+            if (reloadSource != null) reloadSource.Play();
+            if (projectileToDisableOnFire != null) projectileToDisableOnFire.SetActive(true);
         }
     }
 }
